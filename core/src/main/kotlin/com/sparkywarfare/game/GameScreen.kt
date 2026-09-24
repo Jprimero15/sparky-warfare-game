c2203a4af8334b2d8693928795b0596ed8feb980
Jprimero15 <jprimero15@aospa.co>
2026-09-24 21:25:33 +0800

    fix FreeType generator import

--- core/src/main/kotlin/com/sparkywarfare/game/GameScreen.kt
+++ core/src/main/kotlin/com/sparkywarfare/game/GameScreen.kt
@@ -8,7 +8,7 @@ import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.g2d.BitmapFont
-import com.badlogic.gdx.graphics.g2d.FreeTypeFontGenerator
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator
 import com.badlogic.gdx.graphics.g2d.GlyphLayout
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
