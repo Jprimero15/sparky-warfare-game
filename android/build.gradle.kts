5fe1e3d0203e7f487084256395334f8576b76a54
Jprimero15 <jprimero15@aospa.co>
2026-09-24 23:18:39 +0800

    raise Android minimum SDK to 24

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,7 +15,7 @@ android {
 
     defaultConfig {
         applicationId = "com.sparkywarfare.game"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "0.1"
